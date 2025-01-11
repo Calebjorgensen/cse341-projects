c2960508569e96fcc233eb7af1f1f3a060622a2f
Calebjorgensen <121927129+Calebjorgensen@users.noreply.github.com>
2025-01-11 10:21:15 -0800

    adding the correct files

---
+++ controllers/index.js
@@ -0,0 +1,4 @@
+const getName = (req, res) => {
+    res.send('Elle Jorgensen');
+};
+module.exports = { getName };
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Mr Hoser');
-});
+app.use('/', require('./routes'))
 
-const port = 3000;
+app.listen(port, () => {
+    console.log(`Running on port ${port}`)
+})
 
-app.listen(process.env.port || port);
-console.log('Web Server is listening at port ' + (process.env.port || port));
